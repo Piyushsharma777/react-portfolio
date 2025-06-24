6f2b67878ebb43f12c503dff073df9d2a4b9e8a2
Piyushsharma777 <piyushsharma29052004@gmail.com>
2025-06-24 20:44:48 +0530

    first commit

---
+++ src/pages/Work_Experience/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Projects from "../Projects";
+
+const WorkExperience = () => {
+  return (
+    <div>
+      {/* WorkExperience */}
+      <br />
+      <Projects />
+    </div>
+  );
+};
+
+export default WorkExperience;
